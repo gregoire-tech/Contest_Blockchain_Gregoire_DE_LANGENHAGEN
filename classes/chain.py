5c021429bda26185aba538b617015d9908254e99
gregoire-tech <59013478+gregoire-tech@users.noreply.github.com>
2021-07-09 11:59:54 +0200

    generation du hash

--- classes/chain.py
+++ classes/chain.py
@@ -1,11 +1,15 @@
+import hashlib
+from random import random
+
 class Chain():
     
     def __init__(self):
         self.blocs = []
         self.last_transaction_number = 0
         
-    def generate_hash():
-        pass
+    def generate_hash(self):
+        hashed = hashlib.sha3_256(self.str(random.randint(0,1000000000)).encode().hexdigest)
+        return hashed
     
     def verify_hash():
         pass
